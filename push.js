7a3aa8d590d8f8095cc0be15084b92a9d5c0f2e7
HamedDezvarei <43707785+HamedDezvarei@users.noreply.github.com>
2020-06-08 15:57:30 +0430

    fix

--- push.js
+++ push.js
@@ -7,4 +7,12 @@ let vapiKeys= {
 
 push.setVapidDetails('mailto:test@site.com', vapiKeys.publicKey, vapiKeys.privateKey); 
 
-let sub= {}; 
+let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/fMFY_HFD0U8:APA91bEwaWQIsBMTk4jK6V7AmwBmKMz5sVpWoZ-dvnEhfeRzE08E7xVDfyQu_mAILU_ITpqZ1z4liMzpLmXIaY1pHwBoX9YdIPx34I-awi27-sYV1hGpr18cbEIq5oxeQxnn0RswRunJ",
+"expirationTime":null,
+"keys":{
+  "p256dh":"BC83R7DjKvGp1_QNQvXIFZrtFeQuCLNEJDfbLWHsGiqUsHTsfRmZ_2pzZ1Zz36HXDv7-I3_4Jmya-u6V0ILGjhQ",
+"auth":"2f5xYuHrPoHYqPDhIzQSxQ"
+}
+}; 
+
+push.sendNotification(sub, 'test message');
